f3be5c70857953925a59171673e692ec9e3f77f3
PatrycjaWiza <95050810+PatrycjaWiza@users.noreply.github.com>
2022-12-05 22:29:53 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { BrowserRouter } from 'react-router-dom';
 ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
   //basename="/"
-  <BrowserRouter basename="/patrycjawiza.github.io/ReactApp/">
+  <BrowserRouter basename="/ReactApp/">
     <App />
   </BrowserRouter>
   //</React.StrictMode>
